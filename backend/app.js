6893177c745d5ffc5040875847b33f241856ce3f
Mohit Gusain <mohitgusain8671@gmail.com>
2025-05-03 12:43:49 +0530

    Cors Added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { PORT, NODE_ENV } from './config/env.js';
 import workloadRouter from './routes/workload.js';
+import cors from 'cors';
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/workload', workloadRouter);
